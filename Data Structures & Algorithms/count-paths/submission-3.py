70bb064ff3f1db2edc1263d17d466a63dec1addc
Shubham Soni <shubhsoniandro@gmail.com>
2026-04-26 11:56:37 +0530

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        def factorial(n):
+            if n <= 1:
+                return 1
+            return n * factorial(n-1)
+        # if m<n:
+        #     m,n = n,m
+
+        return int(factorial(m+n-2) / (factorial(m-1)* factorial(n-1)) )
+        
